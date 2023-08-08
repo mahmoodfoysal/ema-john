391ce9c3252df325032a8bb7e513e8acdd3d0121
mahmoodfoysal <foysalcse033@gmail.com>
2023-08-08 11:10:49 +0600

    Event handler added parent container

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,6 +4,9 @@ import './Product.css';
 const Product = (props) => {
     console.log(props?.product);
     const {img, name, price, seller, ratings} = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +16,7 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Ratings: {ratings}</p>
             </div>
-            <button className='btn-cart'>Add To Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add To Cart</button>
         </div>
     );
 };
